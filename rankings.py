7b02c096a8aad5d6ebf87d65819ca99b897422c5
ahmed <14140007@gift.edu.pk>
2020-03-04 20:56:26 +0500

    Working on Analysis

--- rankings.py
+++ rankings.py
@@ -1,5 +1,7 @@
 import pandas as pd
+pd.set_option('display.max_rows', None)
+data = pd.read_csv("data.csv", header=0, delimiter='\t')
+data = data[data['Location'] == "United Kingdom"]
 
-data = pd.read_csv("data.csv", header=1, delimiter='\t')
-print(data["Location"][0])
+print(data[['Institution Name', 'Overall Score']])
 
